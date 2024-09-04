c66724679993393ecdae2eb4fd636d58110d802f
slivka <lev22821@gmail.com>
2024-09-04 21:10:54 +0300

    Добавлены посты

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -4,6 +4,11 @@ const Posts = () => {
   return (
     <div>
       <h2>Посты</h2>
+      <ul>
+        <li>Post 1</li>
+        <li>Post 2</li>
+        <li>Post 3</li>
+      </ul>
     </div>
   );
 };
