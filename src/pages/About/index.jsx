5fd5e6ace12d526fe649b806659e3f06cb45b254
uxuiwithlove <veda3000@gmail.com>
2024-09-04 20:49:34 +0300

    Добавлен новый раздел About

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div>
+      <h2>О нас</h2>
+      <span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+        saepe, quos eos quia tempore atque doloribus necessitatibus ex.
+        Voluptates obcaecati ipsum molestiae possimus aliquid perspiciatis
+        expedita laudantium veniam accusantium deleniti! Exercitationem fugit
+        libero sit esse quaerat id facere. Consequuntur qui mollitia eos
+        voluptatibus hic delectus eaque, placeat reprehenderit? Architecto, est?
+      </span>
+    </div>
+  );
+};
+
+export default About;
