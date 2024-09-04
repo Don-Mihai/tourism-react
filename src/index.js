4f0d64c3a8dc3cadbcdd52f2aede0ce247235aed
Your Name <GeraDenis@mail.ru>
2024-09-04 20:49:35 +0300

    create Home page

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Auth from './pages/Auth';
-import Register from './pages/Register';
-import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Auth from "./pages/Auth";
+import Register from "./pages/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Register from "./pages/homePage";
 
 const router = createBrowserRouter([
-    {
-        path: '/auth',
-        element: <Auth />,
-    },
-    {
-        path: '/register',
-        element: <Register />,
-    },
-    {
-        path: '*',
-        element: <div>404 - Page Not Found</div>,
-    },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "*",
+    element: <div>404 - Page Not Found</div>,
+  },
+  {
+    path: "/home",
+    element: <homePage />,
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
-
