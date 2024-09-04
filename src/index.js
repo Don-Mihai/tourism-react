2bddb1fc5250ca2c37628b685b73e3aa3fbf53ba
slivka <lev22821@gmail.com>
2024-09-04 20:49:34 +0300

    добавлен пост

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Auth from './pages/Auth';
-import Register from './pages/Register';
-import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Auth from "./pages/Auth";
+import Register from "./pages/Register";
+import Posts from ".src/pages/Posts";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 const router = createBrowserRouter([
-    {
-        path: '/auth',
-        element: <Auth />,
-    },
-    {
-        path: '/register',
-        element: <Register />,
-    },
-    {
-        path: '*',
-        element: <div>404 - Page Not Found</div>,
-    },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/posts",
+    element: <Posts />,
+  },
+  {
+    path: "*",
+    element: <div>404 - Page Not Found</div>,
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
-
---
+++ src/pages/Posts/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Posts = () => {
+  return (
+    <div>
+      <h2>Посты</h2>
+    </div>
+  );
+};
+
+export default Posts;
