ea8ff8efe440e65b7cfbb32b279f8021f8366b6a
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-09-06 21:58:50 +0300

    register and auth

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,20 +1,53 @@
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Auth.scss';
+import axios from 'axios';
+
+const initialValues = {
+    email: '',
+    password: '',
+};
 
 const Auth = () => {
-  return (
-    <div>
-      <h2>Авторизация</h2>
+    const [formValues, setFormValues] = useState(initialValues);
 
-      <div className="inputs">
-        <input type="text" />
-        <input type="text" />
-      </div>
+    const navigate = useNavigate();
 
-      <div>
-        Нет аккаунта? <Link to="/register">Зарегистрируйтесь</Link>
-      </div>
-    </div>
-  );
+    const onChange = event => {
+        const value = event.target.value;
+        const key = event.target.name;
+        setFormValues({ ...formValues, [key]: value });
+    };
+
+    const auth = async () => {
+        if (formValues.email === '' || formValues.password === '') {
+            alert('Введите обязательные значения!');
+            return;
+        }
+
+        const user = (await axios.get(`http://localhost:3001/users?email=${formValues.email}&password=${formValues.password}`)).data[0];
+
+        if (user) navigate('/home');
+    };
+
+    return (
+        <div className="auth">
+            <h2 className="auth__title">Авторизация</h2>
+            <div className="auth__inputs">
+                <input required onChange={onChange} name="email" value={formValues.email} type="email" className="auth__input" placeholder="Email" />
+                <input required onChange={onChange} name="password" value={formValues.password} type="password" className="auth__input" placeholder="Пароль" />
+            </div>
+            <button onClick={auth} className="auth__button">
+                Войти
+            </button>
+            <div className="auth__footer">
+                Нет аккаунта?{' '}
+                <Link to="/auth" className="auth__link">
+                    Зарегистрируйтесь
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default Auth;
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.scss';
+import axios from 'axios';
 
 const initialValues = {
-    name: 'mihai',
+    name: '',
     email: '',
     password: '',
 };
@@ -11,16 +12,30 @@ const initialValues = {
 const Register = () => {
     const [formValues, setFormValues] = useState(initialValues);
 
-    const onChange = () => {};
+    const navigate = useNavigate();
+
+    const onChange = event => {
+        const value = event.target.value;
+        const key = event.target.name;
+        setFormValues({ ...formValues, [key]: value });
+    };
+
+    const register = async () => {
+        await axios.post('http://localhost:3001/users', formValues);
+        navigate('/home');
+    };
 
     return (
         <div className="register">
             <h2 className="register__title">Регистрация</h2>
             <div className="register__inputs">
-                <input onChange={onChange} value={formValues.name} type="text" className="register__input" placeholder="Имя" />
-                <input onChange={onChange} value={formValues.email} type="email" className="register__input" placeholder="Email" />
-                <input onChange={onChange} value={formValues.password} type="password" className="register__input" placeholder="Пароль" />
+                <input onChange={onChange} name="name" value={formValues.name} type="text" className="register__input" placeholder="Имя" />
+                <input onChange={onChange} name="email" value={formValues.email} type="email" className="register__input" placeholder="Email" />
+                <input onChange={onChange} name="password" value={formValues.password} type="password" className="register__input" placeholder="Пароль" />
             </div>
+            <button onClick={register} className="register__button">
+                Зарегистрироваться
+            </button>
             <div className="register__footer">
                 Уже есть аккаунт?{' '}
                 <Link to="/auth" className="register__link">
