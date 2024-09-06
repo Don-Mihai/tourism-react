37f4771058566d226960285a2f960053e06c230a
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-09-06 19:51:14 +0300

    change

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Register.scss';
 
+const initialValues = {
+    name: 'mihai',
+    email: '',
+    password: '',
+};
+
 const Register = () => {
+    const [formValues, setFormValues] = useState(initialValues);
+
+    const onChange = () => {};
+
     return (
         <div className="register">
             <h2 className="register__title">Регистрация</h2>
             <div className="register__inputs">
-                <input type="text" className="register__input" placeholder="Имя" />
-                <input type="text" className="register__input" placeholder="Email" />
-                <input type="password" className="register__input" placeholder="Пароль" />
+                <input onChange={onChange} value={formValues.name} type="text" className="register__input" placeholder="Имя" />
+                <input onChange={onChange} value={formValues.email} type="email" className="register__input" placeholder="Email" />
+                <input onChange={onChange} value={formValues.password} type="password" className="register__input" placeholder="Пароль" />
             </div>
             <div className="register__footer">
                 Уже есть аккаунт?{' '}
