c6a785e186a3e220b1f95a6661f8bc05b2bac359
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-09-18 18:49:34 +0300

    rating

--- src/pages/HotelsPage/index.jsx
+++ src/pages/HotelsPage/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Card, CardMedia, CardContent, Typography, Button, Rating } from '@mui/material';
 import './HotelsPage.scss';
+import axios from 'axios';
 
 const hotels = [
   {
@@ -8,17 +9,29 @@ const hotels = [
     name: 'Hotel A',
     description: 'A beautiful hotel in Colombo.',
     image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/20/39/92/rayavadee-swimmimg-pool.jpg?w=1200&h=-1&s=1',
+    rating: 5,
   },
   {
     id: 2,
     name: 'Hotel B',
     description: 'A luxurious hotel in Kandy.',
     image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/52/0a/54/manta-pool.jpg?w=1200&h=-1&s=1',
+    rating: 4,
   },
   // Add more hotels as needed
 ];
 
 const HotelsPage = () => {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    getHotels();
+  }, []);
+
+  const getHotels = async () => {
+    const hotels = (await axios.get('http://localhost:3001/hotels')).data;
+    setHotels(hotels);
+  };
   return (
     <div className="hotels-page">
       <h1 className="hotels-page__title">Hotels in Sri Lanka</h1>
@@ -35,6 +48,7 @@ const HotelsPage = () => {
                   {hotel.description}
                 </Typography>
               </CardContent>
+              <Rating className="hotels-page__stars" name="read-only" value={hotel.rating} readOnly />
               <Button variant="contained" color="primary">
                 Book Now
               </Button>
